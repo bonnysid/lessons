966c1e2d931d0e2142fd215c07a510177b1ce646
bonnysid <thezaverimouth@mail.ru>
2019-12-04 20:40:13 +0400

    first commit

---
+++ lesson 1.js
@@ -0,0 +1,87 @@
+'use strict'
+
+//1 урок
+
+/*var leftBorderWidth = 1;
+{
+    let second = 2;
+}
+
+const pi = 3.14;
+
+console.log(second); //не выведет т.к second закрыта скобками
+----------------------------------------------------------------------*/
+
+//2 урок
+
+/*var number = 5;//число
+var string = "Hello!";//string
+var sym = Symbol();
+var boolean = true;// 1 or 0
+null;
+undefined;
+var obj = {};//комплексный(функция)
+
+console.log(4/0);
+console.log('string'*9);
+
+let something;
+console.log(something); //выведет undefined
+
+let persone = {     // obj 
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]); //вывод без точек
+
+let arr = ['plum.png','orange.jpg','apple.bmp']; //массив
+
+console.log(arr[0]);
+---------------------------------------------------------------------------------------------*/
+
+//3 урок
+
+/*alert("Hello World");
+
+let answer = confirm("Are you here?");
+
+console.log(answer); // выводит true или false в зависмости от ответа
+
+
+let answer = prompt("Сколько вам лет?", "18");
+
+console.log(answer);// запрашивает строку где можно писать
+//console.log(typeof(answer)); // тип данных string
+
+------------------------------------------------------------------------------------------------*/
+
+//4 урок
+
+// let answer = +prompt("Есть ли вам 18?", "Да");
+
+// console.log(typeof(answer));
+
+// console.log("OOl" + "get");
+// console.log(4 + "get");
+
+// let incr = 10,
+//     decr = 10;
+
+
+
+// console.log(incr++);//сначала выведет число потом прибавит
+// console.log(--decr);//сначала отнимет потом выведет 
+
+// console.log(5%2);//остаток
+// console.log("2" === 2);//сравнение по типу данных
+// console.log("2" == 2);//сравнение 
+
+// let isChecked = true,
+//     isClose = false;
+
+// console.log(isChecked && !isClose);
+// ------------------------------------------------------------------------------------------------
+
+// 5 урок

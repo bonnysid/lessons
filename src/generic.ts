eef75ac262ba6273496f539d88278b4eaee2c908
bonnysid <bonnysidworker@gmail.com>
2021-03-12 11:11:47 +0400

    add generic

--- src/generic.ts
+++ src/generic.ts
@@ -7,5 +7,9 @@ function reverse<T>(array: T[]): T[] {
     return array.reverse()
 }
 
+const reverse2 = <T>(array: T[]): T[] => {
+    return array.reverse()
+}
+
 reverse(arrayOfNumbers)
 reverse(arrayOfStrings)
